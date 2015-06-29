bf4783bbb2215de17f1347c5fa49c6ababd5676f
Duong Tam Chau <dtc@iuservice.com>
2015-06-29 12:15:40 -0400

    Adapt recommendation and add reverb effect

--- PitchPerfect/SoundConverterViewController.swift
+++ PitchPerfect/SoundConverterViewController.swift
@@ -15,9 +15,16 @@ class SoundConverterViewController: UIViewController {
     let labelHowToStop = "RELEASE button to stop."
     
     @IBOutlet weak var audioConverterLabel: UILabel!
+    @IBOutlet weak var reverbEffectSlider: UISlider!
+    @IBOutlet weak var reverbEffectLabel: UILabel!
     var audioEngine: AVAudioEngine!
     var audioPlayerNode: AVAudioPlayerNode!
     var recordedAudio: RecordedAudio!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshReverbEffectLabel(reverbEffectSlider)
+    }
 
     @IBAction func convertAudioToSlow(sender: UIButton) {
         playAudio(recordedAudio.filePathUrl, rate: 0.5, pitch: 1)
@@ -42,18 +49,27 @@ class SoundConverterViewController: UIViewController {
         audioConverterLabel.text = labelHowToStart
     }
     
+    @IBAction func refreshReverbEffectLabel(sender: UISlider) {
+        reverbEffectLabel.text = "Reverb Effect Value: \(reverbEffectSlider.value)"    }
+    
     func playAudio(audioUrl: NSURL!, rate: Float, pitch: Float) {
         audioEngine = AVAudioEngine()
         audioPlayerNode = AVAudioPlayerNode()
         
-        var pitchNode = AVAudioUnitTimePitch()
+        let pitchNode = AVAudioUnitTimePitch()
         pitchNode.pitch = pitch
         pitchNode.rate = rate
         
+        let reverbNode = AVAudioUnitReverb()
+        reverbNode.loadFactoryPreset(AVAudioUnitReverbPreset.LargeHall)
+        reverbNode.wetDryMix = reverbEffectSlider.value
+        
         audioEngine.attachNode(audioPlayerNode)
         audioEngine.attachNode(pitchNode)
+        audioEngine.attachNode(reverbNode)
         audioEngine.connect(audioPlayerNode, to: pitchNode, format: nil)
-        audioEngine.connect(pitchNode, to: audioEngine.outputNode, format: nil)
+        audioEngine.connect(pitchNode, to: reverbNode, format: nil)
+        audioEngine.connect(reverbNode, to: audioEngine.outputNode, format: nil)
         
         audioPlayerNode.scheduleFile(AVAudioFile(forReading: audioUrl, error: nil), atTime: nil, completionHandler: nil)
         
